620d9d2bc69fe587cd609861ee00254809ce9e60
Haysten Dcosta <haystendc@gmail.com>
2023-10-14 21:29:27 +0530

    README edited and minor changes done

--- main.cpp
+++ main.cpp
@@ -43,35 +43,61 @@ int main()
     std::cout << "\n\t\t\t\t.................>>>>> WELCOME <<<<<.................\t\n";
     std::cout << "\n\t\t\t\t\t  Setting up the system for you !\n";
 
-    std::cout << "\n\n\n\t\t\tSet up a strong password : "; 
-    p = encriptPass();
-    std::cout << "\n\t\t\tRe-Enter password : "; confirm = encriptPass();
-
-    if(confirm == p) { 
-        loading();
-        printHeader();
-        std::cout << "\n\n\n\n\t\t\t\t\t---- SYSTEM SET-UP SUCCESSFUL ----"
-                  << "\n\n\n\n\n\t\t\tPassword successfully set !   \n\t\t\t. . . "; 
-        password = p;
-    }
-    else { 
-        loading_refresh();
-        system("cls");
-        printHeader();
-        std::cout << "\n\n\t\t\tPasswords dont match !  Try Again !!.....\n";
-        std::cout << "\t\t\t"; system("pause");
-        static int count = {};
-        count ++;
-        if((count%5) == 0) {
-            for(int i=15; i>=0; i--) {
-                system("cls");
-                printHeader();
-                std::cout << "\n\t\t\t\t.................>>>>> WELCOME <<<<<.................\t\n";
-                std::cout << "\n\n\n\n\n\n\t\t\t\t\t\tTry Again after : " << i <<"s . . .\n\t\t\t";
-                Sleep(1000);
+    std::cout << "\n\n\n\t\t\tAlready Signed in ? "; std::cin >> confirm;
+    if((confirm == "Y") || (confirm == "y")) {
+        std::ifstream in;
+        std::string pswdStr;
+        std::string searchPassword;
+        in.open("Password.txt");
+        int count = {};
+        std::cout << "\n\n\t\t\tEnter previously entered password : "; std::cin.ignore();
+        getline(std::cin, searchPassword);
+        while(!in.eof() && !in.fail()) {
+            getline(in, pswdStr);
+            if(searchPassword == pswdStr) {
+                count++;
             }
         }
-        main();
+        in.close();
+        if(count == 0) { std::cout << "\n\t\t\tNo such logins found !\n"; goto setNewPass; }
+    }
+    else {
+        setNewPass : 
+        std::cout << "\n\n\n\t\t\tSet up a strong password : "; 
+        p = encriptPass();
+        std::cout << "\n\t\t\tRe-Enter password : "; confirm = encriptPass();
+
+        if(confirm == p) { 
+            loading();
+            printHeader();
+            std::cout << "\n\n\n\n\t\t\t\t\t---- SYSTEM SET-UP SUCCESSFUL ----"
+                      << "\n\n\n\n\n\t\t\tPassword successfully set !   \n\t\t\t. . . "; 
+            password = p;
+
+            //? Storing the password for next login....
+            std::ofstream out;
+            out.open("Password.txt", std::ios::app);
+            out << password << std::endl;
+        }
+        else { 
+            loading_refresh();
+            system("cls");
+            printHeader();
+            std::cout << "\n\n\t\t\tPasswords dont match !  Try Again !!.....\n";
+            std::cout << "\t\t\t"; system("pause");
+            static int count = {};
+            count ++;
+            if((count%5) == 0) {
+                for(int i=15; i>=0; i--) {
+                    system("cls");
+                    printHeader();
+                    std::cout << "\n\t\t\t\t.................>>>>> WELCOME <<<<<.................\t\n";
+                    std::cout << "\n\n\n\n\n\n\t\t\t\t\t\tTry Again after : " << i <<"s . . .\n\t\t\t";
+                    Sleep(1000);
+                }
+            }
+            main();
+        }
     } 
     system("pause");
     DataBase D(password);
